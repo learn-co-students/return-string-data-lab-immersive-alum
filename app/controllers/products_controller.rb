3390974f96e3aa5b9b2b0c0c3f4c7cc63d60e90c
thuyanduong-flatiron <thuyan.duong@flatironschool.com>
2018-07-13 22:26:16 -0400

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,3 +1,38 @@
 class ProductsController < ApplicationController
 
+  def index
+    @products = Product.all
+  end
+
+  def description
+    product = Product.find(params[:id])
+    render plain: product.description
+  end
+
+  def body
+    product = Product.find(params[:id])
+    render json: product
+  end
+
+  def inventory
+    product = Product.find(params[:id])
+    render plain: product.inventory > 0
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.create(product_params)
+    redirect_to products_path
+  end
+
+private
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def product_params
+    params.require(:product).permit(:name, :description)
+  end
+
 end
